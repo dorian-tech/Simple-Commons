06ae61b69584e66744b41b84a7e06524a0174cf5
dorian-tech <50419628+dorian-tech@users.noreply.github.com>
2022-06-05 00:05:41 +0200

    Update CustomizationActivity.kt

--- commons/src/main/kotlin/com/simplemobiletools/commons/activities/CustomizationActivity.kt
+++ commons/src/main/kotlin/com/simplemobiletools/commons/activities/CustomizationActivity.kt
@@ -559,11 +559,6 @@ class CustomizationActivity : BaseSimpleActivity() {
     }
 
     private fun pickPrimaryColor() {
-        if (!packageName.startsWith("com.simplemobiletools.", true) && baseConfig.appRunCount > 50) {
-            finish()
-            return
-        }
-
         curPrimaryLineColorPicker = LineColorPickerDialog(this, curPrimaryColor, true, menu = menu) { wasPositivePressed, color ->
             curPrimaryLineColorPicker = null
             if (wasPositivePressed) {
